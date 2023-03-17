3be0b55c69b809bab659086be430941e410cd214
Irina Lamboglia <irinalamboglia@gmail.com>
2023-03-17 18:51:36 -0300

    Entrega1

---
+++ README.md
@@ -0,0 +1,2 @@
+Nombre: Irina Lamboglia
+Numero de alumno: 18090/3
---
+++ game.py
@@ -0,0 +1,66 @@
+from random import choice, randrange
+from datetime import datetime
+
+# Operadores posibles
+operators = ["+", "-","/","*"]
+
+# Cantidad de cuentas a resolver
+times = 5
+
+# Contador inicial de tiempo.
+# Esto toma la fecha y hora actual.
+init_time = datetime.now()
+print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+
+aux=0
+for i in range(0, times):
+# Se eligen números y operador al azar
+    number_1 = randrange(10)
+    number_2 = randrange(10)
+    operator = choice(operators)
+
+    #se fija que no divida por 0
+    while (operator =="/") and (number_2 == 0):
+        number_2 = randrange(10)
+
+# Se imprime la cuenta.        
+    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+
+# Le pedimos al usuario el resultado
+    result = input("resultado: ")
+    match operator:
+        case "+": 
+             if((number_1 + number_2) == int(result)):
+                 print("correcto")
+                 aux+=1
+             else: 
+                 print("Incorrecto")    
+        case "-": 
+             if((number_1 - number_2) == int(result)):
+                 print("correcto")
+                 aux+=1
+             else: 
+                 print("Incorrecto")        
+        case "/":
+             if((number_1 / number_2) == float(result)):
+                 print("correcto")
+                 aux+=1
+             else: 
+                 print("Incorrecto")    
+        case "*": 
+             if((number_1 * number_2) == int(result)):
+                 print("correcto")
+                 aux+=1    
+             else: 
+                 print("Incorrecto")         
+
+# Al terminar toda la cantidad de cuentas por resolver.
+# Se vuelve a tomar la fecha y la hora.
+end_time = datetime.now()
+
+# Restando las fechas obtenemos el tiempo transcurrido.
+total_time = end_time - init_time
+
+# Mostramos ese tiempo en segundos.
+print(f"\n Tardaste {total_time.seconds} segundos.")
+print(f"Correcto: {aux}, incorrecto: {times-aux}")
